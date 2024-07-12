bd8781166adc6f670604f1e612ee261a0035be17
Godfred <essiengodfred88@gmail.com>
2024-07-12 21:38:26 +0000

    added quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,18 +1,17 @@
 
 function checkAnswer() {
-    const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]');
-    userAnswer["checked"];
-    const feedback = document.getElementById("feedback");
+            const correctAnswer = "4";
+            const userAnswer = document.querySelector('input[name="quiz"]').checked.value;
+            const feedback = document.getElementById("feedback");
 
-    if (userAnswer === correctAnswer) {
-        feedback.textContent = "Correct! Well done.";
-    } else {
-        feedback.textContent = "That's incorrect. Try again!";
-    }
-}
+            if (userAnswer === correctAnswer) {
+                feedback.textContent = "Correct! Well done.";
+            } else {
+                feedback.textContent = "That's incorrect. Try again!";
+            }
+        }
 
-document.getElementById("submit-answer").addEventListener("click", checkAnswer);      
+        document.getElementById("submit-answer").addEventListener("click", checkAnswer);      
 
 
 
