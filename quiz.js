537f2450eb389c577553ee2530c272c135b9a9c7
Godfred <essiengodfred88@gmail.com>
2024-07-12 21:56:28 +0000

    added other files

--- quiz.js
+++ quiz.js
@@ -1,28 +1,21 @@
 
 function checkAnswer() {
-            const correctAnswer = "4";
-            const userAnswer = document.querySelector('input[name="quiz"]').checked.value;
-            const feedback = document.getElementById("feedback");
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]').checked.value;
+    const feedback = document.getElementById("feedback");
 
-            if (userAnswer === correctAnswer) {
-                feedback.textContent = "Correct! Well done.";
-            } else {
-                feedback.textContent = "That's incorrect. Try again!";
-            }
-        }
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
 
-        document.getElementById("submit-answer").addEventListener("click", checkAnswer);      
+document.getElementById("submit-answer").addEventListener("click", checkAnswer); 
 
-
-
-
-
-
-
-
-
-
-
+// let quiz = document.querySelectorAll('input');
+// let feedback = document.querySelector('#feedback');
+// let button = document.querySelector('#submit-answer');
 
 // quiz.forEach( answer => {
 //     answer.addEventListener('click', () => {
