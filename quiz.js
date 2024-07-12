3b844a0d637fb3c6f1086e06b1270a7c0fcf16d8
Godfred <essiengodfred88@gmail.com>
2024-07-12 21:14:00 +0000

    added quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,24 +1,55 @@
 
+const choice1 = document.querySelector('#choice1');
+const choice2 = document.querySelector('#choice2');
+const choice3 = document.querySelector('#choice3');
+
 const quiz =document.querySelectorAll('input');
 const feedback =document.querySelector('#feedback');
 const button =document.querySelector('#submit-answer');
 
-quiz.forEach( answer => {
+let userAnswer = quiz.forEach( answer => {
     answer.addEventListener('click', () => {
-        let userAnswer = answer.value;
-
-        function checkAnswer(){
-            const correctAnswer = "4";
-
-            if (userAnswer === correctAnswer){
-                feedback.textContent = "Correct! Well done.";
-            } else {
-                feedback.textContent = "That's incorrect. Try again!";
-            }
-        }
-
-        button.addEventListener('click', checkAnswer);   
-            
+        return answer.value;
     });
 });
 
+function checkAnswer(){
+    const correctAnswer = "4";
+}
+
+if (userAnswer === correctAnswer()){
+    feedback.textContent = "Correct! Well done.";
+} else {
+    feedback.textContent = "That's incorrect. Try again!";
+}
+
+button.addEventListener('click', checkAnswer);       
+
+
+
+
+
+
+
+
+
+
+
+
+// quiz.forEach( answer => {
+//     answer.addEventListener('click', () => {
+//         let userAnswer = answer.value;
+//         console.log(userAnswer);
+
+//         function checkAnswer(){
+//             const correctAnswer = "4";
+
+//             if (userAnswer === correctAnswer){
+//                 feedback.textContent = "Correct! Well done.";
+//             } else {
+//                 feedback.textContent = "That's incorrect. Try again!";
+//             }
+//         }
+//         button.addEventListener('click', checkAnswer);       
+//     });
+// })
